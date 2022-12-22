9a8246c613fb77e50bbb87f584f47d8db1e5e307
Carlos Diones <carlosdiones28@gmail.com>
2022-12-22 13:12:58 -0300

    Adds ClassVersioning

---
+++ ApiStudiesWithNet5/ClassVersioning/Controllers/AwesomeController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClassVersioning.Controllers
+{
+    [ApiVersion("1.0")]
+    [Route("api/awesome")]
+    public class AwesomeV1Controller : Controller
+    {
+        public IActionResult Get() => Ok("Version 1");
+    }
+    
+    [ApiVersion("2.0")]
+    [Route("api/awesome")]
+    public class AwesomeV2Controller : Controller
+    {
+        public IActionResult Get() => Ok($"Version 2 - {Request.HttpContext.Connection.Id}");
+    }
+}
