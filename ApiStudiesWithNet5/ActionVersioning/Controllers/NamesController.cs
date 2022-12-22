25aa5107285c0f6e1671e6aaca568240c6c64d05
Carlos Diones <carlosdiones28@gmail.com>
2022-12-22 15:46:37 -0300

    Adds ActionVersioning example

---
+++ ApiStudiesWithNet5/ActionVersioning/Controllers/NamesController.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActionVersioning.Controllers
+{
+    [ApiVersion("1.0")]
+    [ApiVersion("2.0")]
+    [Route("api/[controller]")]
+    public class NamesController : Controller
+    {
+        [MapToApiVersion("1.0")]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+
+        [MapToApiVersion("2.0")]
+        public IActionResult Put(string name)
+        {
+            return Ok();
+        }
+    }
+}
